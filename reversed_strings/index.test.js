c16f1de29d00dac79626e3bbf346caa8537d9fe7
Luan F. Silva <luanflorencio87@gmail.com>
2022-08-13 16:42:33 -0300

    initial commit v2

--- reversed_strings/index.test.js
+++ reversed_strings/index.test.js
@@ -1,14 +1,8 @@
 const solution = require('./index');
 
-test('reverse "world" => "dlrow"', () => {
+test('reverse the strings', () => {
     expect(solution('world')).toBe('dlrow');
-});
-test('reverse "hello" => "olleh"', () => {
     expect(solution('hello')).toBe('olleh');
-});
-test('reverse "" => ""', () => {
     expect(solution('')).toBe('');
-});
-test('reverse "h" => "h"', () => {
     expect(solution('h')).toBe('h');
 });
